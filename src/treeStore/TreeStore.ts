9b6214ee7d8d9ab52ce0c175b0437610ca8639e6
Mikhail <hiimisha@mail.ru>
2026-08-20 00:35:21 +0300

    updateItem, addItem

--- src/treeStore/TreeStore.ts
+++ src/treeStore/TreeStore.ts
@@ -1,17 +1,39 @@
 import TTreeItemsData from "../tree/types/types"
 
 export default class TreeStore {
-    private readonly items: TTreeItemsData[]
+    private readonly items = new Map<number | string, TTreeItemsData>()
 
     constructor(items: TTreeItemsData[]) {
-        this.items = items
+        for (const item of items) {
+            this.addItem(item)
+        }
     }
 
     getAll(): TTreeItemsData[] {
-        return this.items
+        return Array.from(this.items.values())
     }
 
     getItem(id: number | string): TTreeItemsData | undefined {
-        return this.items.find(item => item.id === id)
+        return this.items.get(id)
+    }
+
+    addItem(item: TTreeItemsData): void {
+        if (this.items.has(item.id)) {
+            throw new Error(
+                `Элемент с ID "${item.id}" существует`
+            )
+        }
+
+        this.items.set(item.id, item)
+    }
+
+    updateItem(item: TTreeItemsData): void {
+        if (!this.items.has(item.id)) {
+            throw new Error(
+                `Элемента с ID "${item.id}" не существует`
+            )
+        }
+
+        this.items.set(item.id, item)
     }
 }
